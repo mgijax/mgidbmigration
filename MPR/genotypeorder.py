05d9df09762d7295a0f66ed9810e9bc25533316b
Lori Corbani <Lori.Corbani@jax.org>
2005-04-21 19:20:42 +0000

    MPR

--- MPR/genotypeorder.py
+++ MPR/genotypeorder.py
@@ -63,106 +63,6 @@ isComplex = []
 for r in results:
     isComplex.append(r['_Genotype_key'])
 
-# strain ordering
-
-db.sql('select ap._Genotype_key, ap._Allele_key_1, ap._Allele_key_2, p._Strain_key, s.strain ' + \
-	'into #allelepairs ' + \
-	'from GXD_AllelePair ap, GXD_Genotype p, PRB_Strain s ' + \
-	'where ap._Genotype_key = p._Genotype_key ' + \
-	'and p._Strain_key = s._Strain_key ', None)
-
-db.sql('select distinct _Allele_key_1, _Allele_key_2, _Strain_key, strain into #uniqap from #allelepairs', None)
-
-db.sql('select * into #orderbystrain from #uniqap group by _Allele_key_1, _Allele_key_2 having count(*) > 1', None)
-
-results = db.sql('select distinct a._Genotype_key, a._Allele_key_1 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain != "Not Specified" ' + \
-	'and s.strain not like "involves:%" ' + \
-	'and s.strain not like "either:%" ' + \
-	'and s._Strain_key = a._Strain_key ' + \
-	'union ' + \
-	'select distinct a._Genotype_key, a._Allele_key_2 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain != "Not Specified" ' + \
-	'and s.strain not like "involves:%" ' + \
-	'and s.strain not like "either:%" ' + \
-	'and s._Strain_key = a._Strain_key ', 'auto')
-
-isNoPrefix = {}
-for r in results:
-    key = r['_Genotype_key']
-    if not isNoPrefix.has_key(key):
-	isNoPrefix[key] = []
-    isNoPrefix[key].append(r['_Allele_key_1'])
-
-results = db.sql('select distinct a._Genotype_key, a._Allele_key_1 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain like "involves:%" ' + \
-	'and s._Strain_key = a._Strain_key ' + \
-	'union ' + \
-	'select distinct a._Genotype_key, a._Allele_key_2 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain like "involves:%" ' + \
-	'and s._Strain_key = a._Strain_key ', 'auto')
-
-isInvolves = {}
-for r in results:
-    key = r['_Genotype_key']
-    if not isInvolves.has_key(key):
-	isInvolves[key] = []
-    isInvolves[key].append(r['_Allele_key_1'])
-
-results = db.sql('select distinct a._Genotype_key, a._Allele_key_1 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain like "either:%" ' + \
-	'and s._Strain_key = a._Strain_key ' + \
-	'union ' + \
-	'select distinct a._Genotype_key, a._Allele_key_2 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain like "either:%" ' + \
-	'and s._Strain_key = a._Strain_key ', 'auto')
-
-isEither = {}
-for r in results:
-    key = r['_Genotype_key']
-    if not isEither.has_key(key):
-	isEither[key] = []
-    isEither[key].append(r['_Allele_key_1'])
-
-results = db.sql('select distinct a._Genotype_key, a._Allele_key_1 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain = "Not Specified" ' + \
-	'and s._Strain_key = a._Strain_key ' + \
-	'union ' + \
-	'select distinct a._Genotype_key, a._Allele_key_2 ' + \
-	'from #orderbystrain s, #allelepairs a ' + \
-	'where s._Allele_key_1 = a._Allele_key_1 ' + \
-	'and s._Allele_key_2 = a._Allele_key_2 ' + \
-	'and s.strain = "Not Specified" ' + \
-	'and s._Strain_key = a._Strain_key ', 'auto')
-
-isNotSpec = {}
-for r in results:
-    key = r['_Genotype_key']
-    if not isNotSpec.has_key(key):
-	isNotSpec[key] = []
-    isNotSpec[key].append(r['_Allele_key_1'])
-
 results = db.sql('select * from #alleles order by _Allele_key_1, strain', 'auto')
 alleles = {}
 for r in results:
@@ -182,11 +82,8 @@ for a in alleles.keys():
     hemiYSeq = 40
     hemiISeq = 50
     indetSeq = 60
-    elseSeq = 70
-    noPrefixSeq = 80
-    involvesSeq = 90
-    eitherSeq = 100
-    notSpecSeq = 110
+    simpleSeq = 70
+    complexSeq = 80
 
     for r in alleles[a]:
 
@@ -218,29 +115,11 @@ for a in alleles.keys():
 	        sequenceNum = indetSeq
 		indetSeq = indetSeq + 1
 	    else:
-	        sequenceNum = elseSeq
-		elseSeq = elseSeq + 1
-
+	        sequenceNum = simpleSeq
+		simpleSeq = simpleSeq + 1
 	else:
-            if isNoPrefix.has_key(genotype):
-	        if a in isNoPrefix[genotype]:
-	            sequenceNum = noPrefixSeq
-		    noPrefixSeq = noPrefixSeq + 1
-
-	    if isInvolves.has_key(genotype):
-	        if a in isInvolves[genotype]:
-	            sequenceNum = involvesSeq
-		    involvesSeq = involvesSeq + 1
-
-	    if isEither.has_key(genotype):
-	        if a in isEither[genotype]:
-	            sequenceNum = eitherSeq
-		    eitherSeq = eitherSeq + 1
-
-	    if isNotSpec.has_key(genotype):
-	        if a in isNotSpec[genotype]:
-	            sequenceNum = notSpecSeq
-		    notSpecSeq = notSpecSeq + 1
+	    sequenceNum = complexSeq
+	    complexSeq = complexSeq + 1
 
 	fp.write(str(genotype) + TAB)
 	fp.write(str(marker) + TAB)

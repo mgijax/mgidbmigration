a639b5316f2784edeaaa36b3ab131e4493942b68
Lori Corbani <Lori.Corbani@jax.org>
2005-09-01 19:03:50 +0000

    MRI 3.3

--- MPR/allelecombination.py
+++ MPR/allelecombination.py
@@ -6,8 +6,6 @@ import db
 import reportlib
 import mgi_utils
 
-DEBUG = 1
-
 CRT = reportlib.CRT
 SPACE = reportlib.SPACE
 TAB = reportlib.TAB
@@ -26,8 +24,8 @@ fp3 = reportlib.init('allelecombnotetype3', printHeading = 0)
 
 results = db.sql('select g._Genotype_key, alleleState = t1.term, compound = t2.term, allele1 = a1.symbol, allele2 = a2.symbol, ' +
 	'allele1WildType = a1.isWildType, allele2WildType = a2.isWildType, ' + \
-	'mgiID1 = c1.accID, mgiID2 = c2.accID, genotypeID = c3.accID, g.sequenceNum ' + \
-	'from GXD_AllelePair g, VOC_Term t1, VOC_Term t2, ALL_Allele a1, ALL_Allele a2, ACC_Accession c1, ACC_Accession c2, ACC_Accession c3 ' + \
+	'mgiID1 = c1.accID, mgiID2 = c2.accID, genotypeID = c3.accID, g.sequenceNum, m.chromosome ' + \
+	'from GXD_AllelePair g, VOC_Term t1, VOC_Term t2, ALL_Allele a1, ALL_Allele a2, ACC_Accession c1, ACC_Accession c2, ACC_Accession c3, MRK_Marker m ' + \
 	'where g._PairState_key = t1._Term_key ' + \
 	'and g._Compound_key = t2._Term_key ' + \
 	'and g._Allele_key_1 = a1._Allele_key ' + \
@@ -47,11 +45,12 @@ results = db.sql('select g._Genotype_key, alleleState = t1.term, compound = t2.t
 	'and c3._LogicalDB_key = 1 ' + \
 	'and c3.prefixPart = "MGI:" ' + \
 	'and c3.preferred = 1 ' + \
+	'and g._Marker_key = m._Marker_key ' + \
 	'union ' + \
 	'select g._Genotype_key, alleleState = t1.term, compound = t2.term, allele1 = a1.symbol, allele2 = null, ' + \
 	'allele1WildType = a1.isWildType, allele2WildType = 0, ' + \
-	'mgiID1 = c1.accID, mgiID2 = null, genotypeID = c3.accID, g.sequenceNum ' + \
-	'from GXD_AllelePair g, VOC_Term t1, VOC_Term t2, ALL_Allele a1, ACC_Accession c1, ACC_Accession c3 ' + \
+	'mgiID1 = c1.accID, mgiID2 = null, genotypeID = c3.accID, g.sequenceNum, m.chromosome ' + \
+	'from GXD_AllelePair g, VOC_Term t1, VOC_Term t2, ALL_Allele a1, ACC_Accession c1, ACC_Accession c3, MRK_Marker m ' + \
 	'where g._PairState_key = t1._Term_key ' + \
 	'and g._Compound_key = t2._Term_key ' + \
 	'and g._Allele_key_1 = a1._Allele_key ' + \
@@ -66,6 +65,7 @@ results = db.sql('select g._Genotype_key, alleleState = t1.term, compound = t2.t
 	'and c3._LogicalDB_key = 1 ' + \
 	'and c3.prefixPart = "MGI:" ' + \
 	'and c3.preferred = 1 ' + \
+	'and g._Marker_key = m._Marker_key ' + \
 	'order by g._Genotype_key, g.sequenceNum', 'auto')
 
 genotypes = {}
@@ -84,18 +84,22 @@ for g in genotypes.keys():
 
     displayNotes1 = ''
     displayNotes2 = ''
+    displayNotes3 = ''
 
     topType1 = ''
     topType2 = ''
+    topType3 = ''
 
     bottomType1 = ''
     bottomType2 = ''
+    bottomType3 = ''
 
     for r in genotypes[g]:
 
 	genotypeID = r['genotypeID']
         compound = r['compound']
         alleleState = r['alleleState']
+	chr = r['chromosome']
 
         allele1 = r['allele1']
         allele1WildType = r['allele1WildType']
@@ -105,38 +109,115 @@ for g in genotypes.keys():
         allele2WildType = r['allele2WildType']
         mgiID2 = r['mgiID2']
 
-	topType1 = allele1
         if allele1WildType == 1:
-	    topType2 = allele1
-        else:
-	    topType2 = '\Allele(' + mgiID1 + '|' + allele1 + '|)'
+	    topType3 = allele1
+	else:
+	    topType3 = '\Allele(' + mgiID1 + '|' + allele1 + '|)'
 
         if alleleState in ['Homozygous', 'Heterozygous']:
-	    bottomType1 = allele2
             if allele2WildType == 1:
-	        bottomType2 = allele2
+		bottomType3 = allele2
             else:
-	        bottomType2 = '\Allele(' + mgiID2 + '|' + allele2 + '|)'
+	        bottomType3 = '\Allele(' + mgiID2 + '|' + allele2 + '|)'
 
-        if alleleState == 'Hemizygous X-linked':
-            bottomType1 = bottomType2 = 'Y'
+        elif alleleState == 'Hemizygous X-linked':
+            bottomType3 = 'Y'
 
-        if alleleState == 'Hemizygous Y-linked':
-            bottomType1 = bottomType2 = 'X'
+        elif alleleState == 'Hemizygous Insertion':
+            bottomType3 = '0'
 
-        if alleleState == 'Hemizygous Insertion':
-            bottomType1 = bottomType2 = '0'
+        elif alleleState == 'Hemizygous Deletion':
+            bottomType3 = '-'
 
-        if alleleState == 'Hemizygous Deletion':
-            bottomType1 = bottomType2 = '-'
-    
-        if alleleState == 'Indeterminate':
-            bottomType1 = bottomType2 = '?'
+        elif alleleState == 'Indeterminate':
+            bottomType3 = '?'
 
-        if alleleState != 'Unknown':
+        elif (alleleState == 'Hemizygous Y-linked') or (alleleState == 'Hemizygous Insertion' and chr == 'Y'):
+	    if allele1WildType == 1:
+		bottomType3 = allele1
+	    else:
+	        bottomType3 = '\Allele(' + mgiID1 + '|' + allele1 + '|)'
+            topType3 = 'X'
+
+        displayNotes3 = displayNotes3 + topType3 + '/' + bottomType3 + newline
+
+	# if Allele Pair does not have a compound attribute
+
+        if compound == 'Not Applicable':
+
+            if foundTop >= 1 and foundBottom >= 1:
+                displayNotes1 = displayNotes1 + topType1 + '/' + bottomType1 + newline
+                displayNotes2 = displayNotes2 + topType2 + '/' + bottomType2 + newline
+                topType1 = ''
+                topType2 = ''
+                bottomType1 = ''
+                bottomType2 = ''
+                foundTop = 0
+                foundBottom = 0
+
+            topType1 = allele1
+            topType2 = topType3
+            bottomType2 = bottomType3
+
+            if alleleState in ['Homozygous', 'Heterozygous']:
+                bottomType1 = allele2
+            elif (alleleState == 'Hemizygous Y-linked') or (alleleState == 'Hemizygous Insertion' and chr == 'Y'):
+                topType1 = 'X'
+                bottomType1 = allele1
+                topType2 = topType3
+                bottomType2 = bottomType3
+            else:
+                bottomType1 = bottomType3
+
             displayNotes1 = displayNotes1 + topType1 + '/' + bottomType1 + newline
             displayNotes2 = displayNotes2 + topType2 + '/' + bottomType2 + newline
-    
+
+        elif compound == 'Top':
+
+            # new top, new group: process old group
+
+            if foundBottom >= 1:
+                displayNotes1 = displayNotes1 + topType1 + '/' + bottomType1 + newline
+                displayNotes2 = displayNotes2 + topType2 + '/' + bottomType2 + newline
+                topType1 = ''
+                topType2 = ''
+                bottomType1 = ''
+                bottomType2 = ''
+                foundTop = 0
+                foundBottom = 0
+
+            # new top, same group: need space to separate tops
+
+            if foundTop >= 1:
+                topType1 = topType1 + ' '
+                topType2 = topType2 + ' '
+
+            topType1 = topType1 + allele1
+            topType2 = topType2 + '\Allele(' + mgiID1 + '|' + allele1 + '|)'
+
+            foundTop = foundTop + 1
+
+        elif compound == 'Bottom':
+
+            # new bottom, same group: need space to separate bottoms
+
+            if foundBottom >= 1:
+                bottomType1 = bottomType1 + ' '
+                bottomType2 = bottomType2 + ' '
+
+            bottomType1 = bottomType1 + allele1
+
+            if allele1WildType == 1:
+                bottomType2 = bottomType2 + allele1
+            else:
+                bottomType2 = bottomType2 + '\Allele(' + mgiID1 + '|' + allele1 + '|)'
+
+            foundBottom = foundBottom + 1
+
+    if foundTop >=1 and foundBottom >= 1:
+        displayNotes1 = displayNotes1 + topType1 + '/' + bottomType1 + newline
+        displayNotes2 = displayNotes2 + topType2 + '/' + bottomType2 + newline
+
     fp1.write(genotypeID + TAB + displayNotes1 + CRT)
     fp2.write(genotypeID + TAB + displayNotes2 + CRT)
     fp3.write(genotypeID + TAB + displayNotes2 + CRT)

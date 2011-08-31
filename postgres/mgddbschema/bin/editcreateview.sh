5095d2fba906658f6d77d5701fc3e9ab754b35c5
MGI Admin <mgiadmin@jax.org>
2011-08-31 12:41:03 +0000

    ok

--- postgres/mgddbschema/bin/editcreateview.sh
+++ postgres/mgddbschema/bin/editcreateview.sh
@@ -59,6 +59,7 @@ g/convert(varchar(5), tag)/s//cast(tag as varchar(5))/g
 g/convert(char(10), h.event_date, 101)/s//cast(h.event_date as char(10))/g
 g/convert(varchar(10), r._Class_key)/s//cast(r._Class_key as varchar(10))/g
 g/convert(varchar(10), r._Refs_key)/s//cast(r._Refs_key as varchar(10))/g
+g/ltrim(str(l.size,10,2))/s//to_char(l.size, '9999.99')/g
 /cat
 d
 a

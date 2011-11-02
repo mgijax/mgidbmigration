f6a3ea0dc5ecec853612ab73084e4f95f40d9ac5
Lori Corbani <Lori.Corbani@jax.org>
2011-11-02 16:19:30 +0000

    ok

---
+++ postgres/mgddbschema/bin/comments.py
@@ -0,0 +1,47 @@
+#!/usr/local/bin/python
+
+#
+# create .txt file of:
+#
+#	MGI_Table.table_name
+#	MGI_Tables.description
+#
+
+import sys 
+import os
+import string
+import db
+import mgi_utils
+
+#
+# Main
+#
+
+user = os.environ['MGD_DBUSER']
+passwordFileName = os.environ['MGD_DBPASSWORDFILE']
+db.set_sqlUser(user)
+db.set_sqlPasswordFromFile(passwordFileName)
+
+db.useOneConnection(1)
+
+fp = open('comments.txt', 'w')
+
+results = db.sql('select table_name, description from MGI_Tables', 'auto')
+
+for r in results:
+
+    tableName = r['table_name']
+    description = r['description']
+
+    if description != None:
+    	description = string.replace(description, "'", "''")
+
+    fp.write("COMMENT ON TABLE mgd." + \
+	tableName + \
+	" IS '" + \
+	mgi_utils.prvalue(description) + \
+	"';\n")
+
+fp.close()
+db.useOneConnection(0)
+

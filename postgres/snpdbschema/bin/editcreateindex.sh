3796548dd62dd87aa68381ae68c9d54b98a354de
Lori Corbani <Lori.Corbani@jax.org>
2012-05-14 11:53:11 +0000

    postgres/snp

--- postgres/snpdbschema/bin/editcreateindex.sh
+++ postgres/snpdbschema/bin/editcreateindex.sh
@@ -26,8 +26,18 @@ g/ source/s// ./g
 g/nonclustered /s///g
 g/clustered /s///g
 g/idx/s//${t}/g
+g/ on seg0/s//;/g
+g/ on seg2/s//;/g
+g/ on seg3/s//;/g
+g/ on seg4/s//;/g
+g/ on seg5/s//;/g
+g/ on seg6/s//;/g
+g/ on seg7/s//;/g
+g/ on seg8/s//;/g
 g/ on seg9/s//;/g
 g/ on seg10/s//;/g
+g/ on seg11/s//;/g
+g/ on seg12/s//;/g
 g/ on seg1/s//;/g
 g/ on /s// on snp./g
 g/^go/s///g

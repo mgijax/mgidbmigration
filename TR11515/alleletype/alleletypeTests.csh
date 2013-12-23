2b5d5f5a00628ec7969f5c78d4667a54545692eb
MGI Admin <mgiadmin@jax.org>
2013-12-23 17:09:15 +0000

    TR11515/allele stuff

---
+++ TR11515/alleletype/alleletypeTests.csh
@@ -0,0 +1,30 @@
+#!/bin/csh -fx
+
+#
+# Migration for TR11515
+#
+#
+
+###----------------------###
+###--- initialization ---###
+###----------------------###
+
+if ( ${?MGICONFIG} == 0 ) then
+	setenv MGICONFIG /usr/local/mgi/live/mgiconfig
+endif
+
+source ${MGICONFIG}/master.config.csh
+
+env | grep MGD
+
+# start a new log file for this migration, and add a datestamp
+
+setenv SQLLOG $0.log
+rm -rf ${SQLLOG}
+touch ${SQLLOG}
+
+./alleletypeTests.py | tee -a ${SQLLOG}
+
+date | tee -a ${SQLLOG}
+echo "--- Finished" | tee -a ${SQLLOG}
+
---
+++ TR11515/alleletype/alleletypeTests.py
@@ -0,0 +1,79 @@
+#!/usr/local/bin/python
+
+'''
+#
+# read kim's specific allele test
+#
+'''
+ 
+import sys 
+import os
+import db
+import reportlib
+import mgi_utils
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+#
+# main
+#
+
+db.useOneConnection(1)
+
+inFile = open('/mgi/all/wts_projects/11500/11515/AlleleTypeMigrationTests.txt', 'r')
+
+for line in inFile.readlines():
+	tokens = line[:-1].split('\t')
+	symbol = tokens[0]
+	mgiID = tokens[1]
+	inGterm = tokens[2]
+	subTypes = tokens[3].split(', ')
+	#print symbol, mgiID, inGterm, subTypes
+
+	sql = '''select a.symbol, gt.term as outGterm, st.term as outSterm
+		from ALL_Allele a, ACC_Accession aa, VOC_Term gt, VOC_Annot va, VOC_Term st
+		where aa.accID = '%s'
+		and aa._MGIType_key = 11
+		and aa._Object_key = a._Allele_key
+		and a._Allele_Type_key = gt._Term_key
+		and a._Allele_key = va._Object_key
+		and va._AnnotType_key = 1014
+		and va._Term_key = st._Term_key
+
+		union
+
+		select a.symbol, gt.term as outGterm, 'none'
+		from ALL_Allele a, ACC_Accession aa, VOC_Term gt
+		where aa.accID = '%s'
+		and aa._MGIType_key = 11
+		and aa._Object_key = a._Allele_key
+		and a._Allele_Type_key = gt._Term_key
+		and not exists (select 1 from VOC_Annot va
+			where a._Allele_key = va._Object_key
+			and va._AnnotType_key = 1014)
+		'''
+
+	value = 'pass'
+
+	#print sql
+	results = db.sql(sql % (mgiID, mgiID), 'auto')
+
+	if len(results) == 0:
+		value = 'fail'
+	else:
+		for r in results:
+			if r['outGterm'] != inGterm:
+				value = 'fail'
+			if r['outSterm'] not in subTypes:
+				value = 'fail'
+
+	print value, symbol, mgiID, inGterm, subTypes
+
+inFile.close()
+
+db.useOneConnection(0)
+

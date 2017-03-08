98fdeef752336613387261ec77c5c5443d8fc8c0
Lori Corbani <lori.corbani@jax.org>
2017-03-08 10:25:18 -0500

    TR12540/Disease Ontology (DO)

---
+++ TR12540/omimhpo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd `dirname $0` 
+
+if [ "${MGICONFIG}" = "" ]
+then
+    MGICONFIG=/usr/local/mgi/live/mgiconfig
+    export MGICONFIG
+fi
+
+. ${MGICONFIG}/master.config.sh
+
+LOG=$0.log
+rm -rf $LOG
+touch $LOG
+ 
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 >> $LOG
+
+select a3.accID, v._annot_key, v._annottype_key, v._object_key,v._term_key, v._qualifier_key
+from VOC_Annot v, ACC_Accession a3
+where v._AnnotType_key = 1024
+and v._Object_key = a3._Object_key
+and a3._MGIType_key = 13
+and a3._LogicalDB_key =  191 
+and a3.preferred = 1 
+order by a3.accID
+;
+
+EOSQL
+

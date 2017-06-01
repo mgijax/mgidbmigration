622f5ba68fab77a09e039003e13dc12ee26ededd
Lori Corbani <lori.corbani@jax.org>
2017-06-01 09:02:23 -0400

    TR12250/Lit Triage

--- LitTriage/datasets/datasets.csh
+++ LitTriage/datasets/datasets.csh
@@ -22,9 +22,25 @@ date | tee -a $LOG
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 EOSQL
 
+setenv COLDELIM "|"
+setenv LINEDELIM  "\n"
+
 date | tee -a ${LOG}
 echo 'migrating data sets to workflow'
 ./datasets.py | tee -a $LOG || exit 1
+
+${PG_MGD_DBSCHEMADIR}/table/BIB_Workflow_Status_truncate.object | tee -a ${LOG}
+${PG_MGD_DBSCHEMADIR}/table/BIB_Workflow_Tag_truncate.object | tee -a ${LOG}
+
+${PG_MGD_DBSCHEMADIR}/index/BIB_Workflow_Status_drop.object | tee -a ${LOG}
+${PG_MGD_DBSCHEMADIR}/index/BIB_Workflow_Tag_drop.object | tee -a ${LOG}
+
+${PG_DBUTILS}/bin/bcpin.csh ${PG_DBSERVER} ${PG_DBNAME} BIB_Workflow_Status ${DBUTILS}/mgidbmigration/LitTriage/datasets wf_status_chosen.bcp ${COLDELIM} ${LINEDELIM} mgd | tee -a ${LOG}
+${PG_DBUTILS}/bin/bcpin.csh ${PG_DBSERVER} ${PG_DBNAME} BIB_Workflow_Status ${DBUTILS}/mgidbmigration/LitTriage/datasets wf_status_indexed.bcp ${COLDELIM} ${LINEDELIM} mgd | tee -a ${LOG}
+${PG_DBUTILS}/bin/bcpin.csh ${PG_DBSERVER} ${PG_DBNAME} BIB_Workflow_Status ${DBUTILS}/mgidbmigration/LitTriage/datasets wf_status_rejected.bcp ${COLDELIM} ${LINEDELIM} mgd | tee -a ${LOG}
+
+${PG_MGD_DBSCHEMADIR}/index/BIB_Workflow_Status_create.object | tee -a ${LOG}
+${PG_MGD_DBSCHEMADIR}/index/BIB_Workflow_Tag_create.object | tee -a ${LOG}
 
 date | tee -a $LOG
 
--- LitTriage/datasets/datasets.py
+++ LitTriage/datasets/datasets.py
@@ -9,14 +9,286 @@
 import sys 
 import os
 import db
-import reportlib
+import mgi_utils
 
 db.setTrace()
 
-CRT = reportlib.CRT
-SPACE = reportlib.SPACE
-TAB = reportlib.TAB
-PAGE = reportlib.PAGE
+wf_status = '%s|%s|%s|%s|1|1001|1001|%s|%s\n'
+
+wf_tag = '%s|%s|%s|%s|%s|%s|%s\n'
+
+currentDate = mgi_utils.date('%m/%d/%Y')
+
+#
+# ap/gxd/go/qtl : indexed
+#
+def apgxdgoqtl_indexed():
+   #
+   # selected : any
+   # used : true
+   # not used : false
+   # never used : any
+   # incomplete : any
+   #
+   # wf_status = Indexed
+   #
+
+   global assocStatusKey
+
+   wf_status_bcp = open('wf_status_indexed.bcp', 'w+')
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where exists (select 1 from MGI_Reference_Allele_View gi where gi._Refs_key = r._Refs_key)
+	order by r.jnumID
+	''' % (apKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], indexedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where ((exists (select 1 from GXD_Index gi where gi._Refs_key = r._Refs_key)
+       	  or exists (select 1 from GXD_Assay ga where ga._Refs_key = r._Refs_key)))
+	order by r.jnumID
+	''' % (gxdKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], indexedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where exists (select 1 from VOC_Evidence e, VOC_Annot a
+           where e._Refs_key = r._Refs_key
+	   and e._Annot_key = a._Annot_key
+	   and a._AnnotType_key = 1000)
+	order by r.jnumID
+	''' % (goKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], indexedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where exists (select 1 from MLD_Expts gi where gi._Refs_key = r._Refs_key
+           and gi.exptType in ('TEXT', 'TEXT-QTL', 'TEXT-QTL-Candidate Genes', 'TEXT-Congenic', 'TEXT-Meta Analysis'))
+	order by r.jnumID
+	''' % (qtlKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], indexedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   wf_status_bcp.close()
+
+#
+# ap/gxd/go/qtl : chosen
+#
+def apgxdgoqtl_chosen():
+   #
+   # selected : true
+   # used : false
+   # not used : true
+   # never used : false
+   # incomplete : any
+   #
+   # wf_status = Chosen
+   #
+
+   global assocStatusKey
+
+   wf_status_bcp = open('wf_status_chosen.bcp', 'w+')
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r, BIB_DataSet_Assoc dbsa
+        where dbsa._DataSet_key in (1002)
+	and dbsa._Refs_key = r._Refs_key
+	and dbsa.isNeverUsed = 0
+        and not exists (select 1 from MGI_Reference_Allele_View gi where gi._Refs_key = dbsa._Refs_key)
+	order by r.jnumID
+	''' % (apKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], chosenKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r, BIB_DataSet_Assoc dbsa
+        where dbsa._DataSet_key in (1004)
+	and dbsa._Refs_key = r._Refs_key
+	and dbsa.isNeverUsed = 0
+        and not ((exists (select 1 from GXD_Index gi where gi._Refs_key = r._Refs_key)
+       	  or exists (select 1 from GXD_Assay ga where ga._Refs_key = r._Refs_key)))
+	order by r.jnumID
+	''' % (gxdKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], chosenKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r, BIB_DataSet_Assoc dbsa
+        where dbsa._DataSet_key in (1005)
+	and dbsa._Refs_key = r._Refs_key
+	and dbsa.isNeverUsed = 0
+        and not exists (select 1 from VOC_Evidence e, VOC_Annot a
+           where e._Refs_key = r._Refs_key
+	   and e._Annot_key = a._Annot_key
+	   and a._AnnotType_key = 1000)
+	order by r.jnumID
+	''' % (goKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], chosenKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r, BIB_DataSet_Assoc dbsa
+        where dbsa._DataSet_key in (1011)
+	and dbsa._Refs_key = r._Refs_key
+	and dbsa.isNeverUsed = 0
+        and not exists (select 1 from MLD_Expts gi where gi._Refs_key = r._Refs_key
+           and gi.exptType in ('TEXT', 'TEXT-QTL', 'TEXT-QTL-Candidate Genes', 'TEXT-Congenic', 'TEXT-Meta Analysis'))
+	order by r.jnumID
+	''' % (qtlKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], chosenKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   wf_status_bcp.close()
+
+#
+# ap/gxd/go/qtl : rejected
+#
+def apgxdgoqtl_rejected():
+   #
+   # selected : false
+   # used : false
+   # not used : any
+   # never used : any
+   # incomplete : any
+   #
+   # wf_status = Rejected
+   #
+
+   global assocStatusKey
+
+   wf_status_bcp = open('wf_status_rejected.bcp', 'w+')
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where not exists (select 1 from BIB_DataSet_Assoc dbsa
+            where dbsa._dataset_key in (1002)
+	    and r._Refs_key = dbsa._Refs_key
+	    )
+	and not exists (select 1 from MGI_Reference_Allele_View gi where gi._Refs_key = r._Refs_key)
+	order by r.jnumID
+	''' % (apKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], rejectedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where not exists (select 1 from BIB_DataSet_Assoc dbsa
+            where dbsa._dataset_key in (1004)
+	    and r._Refs_key = dbsa._Refs_key
+	    )
+        and not ((exists (select 1 from GXD_Index gi where gi._Refs_key = r._Refs_key)
+       	  or exists (select 1 from GXD_Assay ga where ga._Refs_key = r._Refs_key)))
+	order by r.jnumID
+	''' % (gxdKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], rejectedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where not exists (select 1 from BIB_DataSet_Assoc dbsa
+            where dbsa._dataset_key in (1005)
+	    and r._Refs_key = dbsa._Refs_key
+	    )
+        and not exists (select 1 from VOC_Evidence e, VOC_Annot a
+           where e._Refs_key = r._Refs_key
+	   and e._Annot_key = a._Annot_key
+	   and a._AnnotType_key = 1000)
+	order by r.jnumID
+	''' % (goKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], rejectedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where not exists (select 1 from BIB_DataSet_Assoc dbsa
+            where dbsa._dataset_key in (1011)
+	    and r._Refs_key = dbsa._Refs_key
+	    )
+        and not exists (select 1 from MLD_Expts gi where gi._Refs_key = r._Refs_key
+           and gi.exptType in ('TEXT', 'TEXT-QTL', 'TEXT-QTL-Candidate Genes', 'TEXT-Congenic', 'TEXT-Meta Analysis'))
+	order by r.jnumID
+	''' % (qtlKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], rejectedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   wf_status_bcp.close()
+
+#
+# ap/Rejected
+#
+def ap_rejected():
+   #
+   # selected : any
+   # used : false
+   # not used : any
+   # never used : true
+   # incomplete : any
+   #
+   # wf_status = Rejected
+   #
+
+   global assocStatusKey
+
+   wf_status_bcp = open('wf_status_rejected.bcp', 'w+')
+
+   querySQL = '''
+        select distinct r._Refs_key, r.jnumID, %s as groupKey
+        from BIB_Citation_Cache r
+        where exists (select 1 from BIB_DataSet_Assoc dbsa
+            where dbsa._dataset_key in (1002)
+	    and r._Refs_key = dbsa._Refs_key
+	    and dbsa.isNeverUsed = 1
+	    )
+	and not exists (select 1 from MGI_Reference_Allele_View gi where gi._Refs_key = r._Refs_key)
+	order by r.jnumID
+	''' % (apKey)
+   results = db.sql(querySQL, 'auto')
+   for r in results:
+   	wf_status_bcp.write(wf_status % (assocStatusKey, r['_Refs_key'], r['groupKey'], rejectedKey, currentDate, currentDate))
+	assocStatusKey += 1
+
+   wf_status_bcp.close()
 
 #
 # Main
@@ -24,21 +296,78 @@ PAGE = reportlib.PAGE
 
 db.useOneConnection(1)
 
-inFile = open('', 'r')
-lineNum = 0
-for line in inFile.readlines():
-	lineNum = lineNum + 1
+assocStatusKey = 1
 
-	tokens = line[:-1].split('\t')
+apKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Group' and v._Vocab_key = t._Vocab_key and t.term = 'Alleles & Phenotypes'
+''')[0]['_Term_key']
 
-fp = reportlib.init(sys.argv[0], printHeading = None)
+gxdKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Group' and v._Vocab_key = t._Vocab_key and t.term = 'Expression'
+''')[0]['_Term_key']
 
-results = db.sql(cmd, 'auto')
+goKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Group' and v._Vocab_key = t._Vocab_key and t.term = 'Gene Ontology'
+''')[0]['_Term_key']
 
-for r in results:
-    fp.write(r['item'] + CRT)
+qtlKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Group' and v._Vocab_key = t._Vocab_key and t.term = 'QTL'
+''')[0]['_Term_key']
 
-inFile.close()
-reportlib.finish_nonps(fp)	# non-postscript file
+indexedKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Status' and v._Vocab_key = t._Vocab_key and t.term = 'Indexed'
+''')[0]['_Term_key']
+
+chosenKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Status' and v._Vocab_key = t._Vocab_key and t.term = 'Chosen'
+''')[0]['_Term_key']
+
+rejectedKey = db.sql('''
+select t._Term_key from VOC_Vocab v, VOC_Term t 
+where v.name = 'Workflow Status' and v._Vocab_key = t._Vocab_key and t.term = 'Rejected'
+''')[0]['_Term_key']
+
+#
+# ap/gxd/go/qtl
+# Indexed, Chosen, Rejected
+#
+apgxdgoqtl_indexed()
+apgxdgoqtl_chosen()
+apgxdgoqtl_rejected()
+
+#
+# ap/Rejected
+#
+ap_rejected()
+
+#
+# ap/AP_Incomplete
+#
+
+#
+# gxd/GXD_Loads
+#
+
+#
+# go/Rejected
+#
+
+#
+# Tumor
+# Indexed, Rejected, Fully coded
+#
+
+#inFile = open('', 'r')
+#lineNum = 0
+#for line in inFile.readlines():
+#	lineNum = lineNum + 1
+#	tokens = line[:-1].split('|')
+
 db.useOneConnection(0)
 

1c1957db859294c20f971a772bdc10f265d3bc04
Lori Corbani <lori.corbani@jax.org>
2017-07-11 06:26:02 -0400

    TR12250/Lit Triage

--- LitTriage/jfilescanner/jfilescanner.py
+++ LitTriage/jfilescanner/jfilescanner.py
@@ -49,7 +49,7 @@ for jfilePath in os.listdir(parentDir):
     for pdfFile in os.listdir(fullFilePath):
 
         #if not pdfFile.startswith('J') or not pdfFile.endswith('.pdf'):
-        if not pdfFile.startswith('J24') or not pdfFile.endswith('.pdf'):
+        if not pdfFile.startswith('J240') or not pdfFile.endswith('.pdf'):
             continue
 
         jfilecount += 1

813ad9751890aae5526c5540d97f81305dff93c5
Lori Corbani <lori.corbani@jax.org>
2017-10-06 09:17:35 -0400

    TR12250/Lit Triage

--- LitTriage/jfilescanner/jfilescanner2.py
+++ LitTriage/jfilescanner/jfilescanner2.py
@@ -62,36 +62,30 @@ for mgifilePath in os.listdir(masterTriageDir):
 	results = refLookup[mgiID]
 	refsKey = results[0]
 
-	pdf = PdfParser.PdfParser(fullpdfFile)
-	extractedText = pdf.getText()
-	if extractedText != None:
-            extractedText = re.sub(r'[^\x00-\x7F]','', extractedText)
-            extractedText = extractedText.replace('\\', '\\\\')
-            extractedText = extractedText.replace('\n', '\\n')
-            extractedText = extractedText.replace('\r', '\\r')
-            extractedText = extractedText.replace('|', '\\n')
-            extractedText = extractedText.replace("'", "''")
-	    #sql += '''
-#
-#	    	    update BIB_Workflow_Data 
-#		    set hasPDF = 1, 
-#		    extractedText = E'%s' where _Refs_key = %s;
-#
-#		    ''' % (extractedText, refsKey)
-	    db.sql('''
+	try:
+	    pdf = PdfParser.PdfParser(fullpdfFile)
+	    extractedText = pdf.getText()
+	    if extractedText != None:
+                extractedText = re.sub(r'[^\x00-\x7F]','', extractedText)
+                extractedText = extractedText.replace('\\', '\\\\')
+                extractedText = extractedText.replace('\n', '\\n')
+                extractedText = extractedText.replace('\r', '\\r')
+                extractedText = extractedText.replace('|', '\\n')
+                extractedText = extractedText.replace("'", "''")
+	        db.sql('''
 	    	    update BIB_Workflow_Data 
 		    set hasPDF = 1, 
 		    extractedText = E'%s' where _Refs_key = %s
 		    ''' % (extractedText, refsKey), None)
-	else:
-	    #sql += '''
-#
-#	    	    update BIB_Workflow_Data set hasPDF = 1 where _Refs_key = %s;
-#
-#		    ''' % (refsKey)
-	    db.sql('''
-	    	    update BIB_Workflow_Data set hasPDF = 1 where _Refs_key = %s
-		    ''' % (refsKey), None)
+	    else:
+	        db.sql('''
+	    	        update BIB_Workflow_Data set hasPDF = 1 where _Refs_key = %s
+		        ''' % (refsKey), None)
+	except:
+	        db.sql('''
+	    	        update BIB_Workflow_Data set hasPDF = 1 where _Refs_key = %s
+		        ''' % (refsKey), None)
+
         db.commit()
         mgifilecount += 1
 	print 'successful: ', mgiID, fullpdfFile

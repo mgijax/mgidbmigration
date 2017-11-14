ed3bb83bdcff015e2b052de1a103afcbc8d8bde2
Lori Corbani <lori.corbani@jax.org>
2017-11-14 13:53:34 -0500

    TR12662/driver notes

--- GMC/drivercheck.csh
+++ GMC/drivercheck.csh
@@ -23,8 +23,8 @@ date | tee -a $LOG
  
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 
-select a.symbol as organizer, m.symbol as participant,
-c.name, t1.name as vocab, t2.term as relterm, t3.term as qualifier, t4.term as evidence,  b.jnumID
+--c.name, t1.name as vocab, t2.term as relterm, t3.term as qualifier, t4.term as evidence,  b.jnumID
+select a.symbol as organizer, m.symbol as participant
 from MGI_Relationship r, MGI_Relationship_Category c, ALL_Allele a, MRK_Marker m, 
 VOC_Vocab t1, VOC_Term t2, VOC_Term t3, VOC_Term t4, BIB_Citation_Cache b
 where r._Category_key = 1006
--- GMC/drivernotes.py
+++ GMC/drivernotes.py
@@ -48,41 +48,33 @@ def doMouse():
 
 #
 # non-mouse : marker = yes, mol note = yes
+# testing using rat
 #
 def doNonMouse1():
     global relKey
 
     sql = '''
-WITH nonmouse AS (
-select distinct a._allele_key, a.symbol, n._object_key, substring(c.note,1,25) as note, b.jnumID
-from mgi_note n, mgi_notechunk c, all_allele a, mgi_reference_assoc r, bib_citation_cache b
-where n._notetype_key = 1034 
-and n._note_key = c._note_key
-and n._object_key = a._allele_key
-and a._allele_key = r._object_key
-and r._mgitype_key = 11
-and r._refassoctype_key in (1012)
-and r._refs_key = b._refs_key
-and (
-        not exists (select 1 from mrk_marker m where c.note = m.symbol)
-	or a._Allele_Type_key = 847116
-        or a.symbol like 'Tg%'
-        or a.symbol like 'Gt(ROSA)%'
-        or a.symbol like 'Hprt<%'
-        or a.symbol like 'Col1a1<%'
-    )
-)
-(
-
-select distinct nm.symbol as allelesymbol, m.symbol, m._organism_key
-from nonmouse nm, mrk_marker m, mgi_note nn, mgi_notechunk cc
-where nm.note = m.symbol
-and m._organism_key != 1
-and nm._object_key = nn._object_key
-and nn._notetype_key = 1021
-and nn._note_key = cc._note_key
-)
-order by nm.symbol
+	WITH nonmouse AS (
+	select distinct a._allele_key, a._Allele_Type_key, a.symbol, r._Refs_key, c.note
+	from mgi_note n, mgi_notechunk c, all_allele a, mgi_reference_assoc r
+	where n._notetype_key = 1034 
+	and n._note_key = c._note_key
+	and n._object_key = a._allele_key
+	and c.note not like 'unknown%'
+	and a._Refs_key = r._Refs_key
+	and r._mgitype_key = 11
+	and r._refassoctype_key in (1012)
+	)
+	select n.*, m._marker_key, m._organism_key
+	from nonmouse n, mrk_marker m
+	where n.note = m.symbol
+	and n.note = m.symbol
+	and m._organism_key = 40
+	and (n._Allele_Type_key != 847116
+        	or n.symbol like 'Gt(ROSA)%'
+        	or n.symbol like 'Hprt<%'
+        	or n.symbol like 'Col1a1<%'
+	    )
 	'''
 
     results = db.sql(sql, 'auto')
@@ -102,7 +94,7 @@ db.useOneConnection(1)
 relBcp = open('MGI_Relationship.bcp', 'w')
 
 doMouse()
-#doNonMouse1()
+doNonMouse1()
 
 #
 #

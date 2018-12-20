57b9b34bb09e2db51dd89eb015980b180fb8565b
Lori Corbani <lori.corbani@jax.org>
2018-12-20 12:28:15 -0500

    TR12963/teleuse-to-mgd_java_api

--- tr12963/variant/variant.py
+++ tr12963/variant/variant.py
@@ -5,6 +5,35 @@
 # Report:
 #       load Meiyee's spreadsheet
 #
+#
+# 1.  mgi id marker
+# 2.  marker symbol
+# 3.  strain name			: var_variant._strain_key
+# 4.  pubmedid				: acc_accession:  _mgitype_key = ?, ldb = 29
+# 5.  J:				: mgi_reference_assoc: _mgitype_key = ?, _object_key = _refs_key
+# 6.  mgi id allele			: var_variant._allele_key
+# 7.  allele symbol
+# 8.  molecular note			: mgi_note: _mgitype_key = ?, _notetype_key = ?
+# 9.  chromosome (format is in "chr")
+# 10. start coordinate			: map
+# 11. end coordinate			: map
+# 12. ref_allele			: var_sequence.referenceSequence
+# 13. alt_allele			: var_sequence.variantSequence
+# 14. jannovar_hgvs 			: var_variant.description
+# 15. jannovar_functional_class		: lookup terms in SO vocab (21)
+#
+# database fields not in spreadsheet/but need to be set
+#
+# var_variant._sourcevariant_key = ?
+# var_variant.isReviewed = ?
+#
+# var_sequence
+# _sequence_type_key = ? where voc_term._vocab_key = 21 (RNA, DNA, Polypeptide, Not Loaded)
+#
+# var_effect, from SO
+#
+# var_type, from SO
+#
 '''
  
 import sys 

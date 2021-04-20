326581cdba05fb93f95cb66f2f8fb4da9bf448a8
Lori Corbani <lori.corbani@jax.org>
2021-04-20 11:07:30 -0400

    TR13349/littriage/testing

--- tr13349/littriage/testdaily.sh
+++ tr13349/littriage/testdaily.sh
@@ -11,7 +11,7 @@ cd `dirname $0`
 #dumpTableData.csh mgi-testdb4 lec mgd BIB_Workflow_Relevance /home/mgi/dataload/littriageload-trunk/bin/BIB_Workflow_Relevance.bcp "|"
 #dumpTableData.csh mgi-testdb4 lec mgd BIB_Workflow_Status /home/mgi/dataload/littriageload-trunk/bin/BIB_Workflow_Status.bcp "|"
 
-cd ${DATALOADSOUTPUT}/littriageload
+cd ${DATALOADSOUTPUT}/mgi/littriageload
 rm -rf lec.tar
 scp bhmgiapp01:/data/loads/mgi/littriageload/lec.tar .
 cd input
@@ -23,10 +23,10 @@ ${LITTRIAGELOAD}/bin/littriageload.sh
 cd ${MGI_LIVE}/dataload/littriageload-trunk/bin
 testRelevance.sh
 testStatus.sh
-cp testRelevance.sh.log ${DATALOADSOUTPUT}/littriageload/output
-cp testStatus.sh.log ${DATALOADSOUTPUT}/littriageload/output
-cp ${DATALOADSOUTPUT}/littriageload/output/* ${DATALOADSOUTPUT}/littriageload/output
-cp ${DATALOADSOUTPUT}/littriageload/logs/* ${DATALOADSOUTPUT}/littriageload/logs
+cp testRelevance.sh.log ${DATALOADSOUTPUT}/mgi/littriageload/output
+cp testStatus.sh.log ${DATALOADSOUTPUT}/mgi/littriageload/output
+cp ${DATALOADSOUTPUT}/mgi/littriageload/output/* ${DATALOADSOUTPUT}/mgi/littriageload/output
+cp ${DATALOADSOUTPUT}/mgi/littriageload/logs/* ${DATALOADSOUTPUT}/mgi/littriageload/logs
 
 cd ${MGI_LIVE}/qcreports_db
 source ./Configuration

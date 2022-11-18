e196a214fcbc282ab525bfb9b98c2a93080790a0
Lori Corbani <lori.corbani@jax.org>
2022-11-18 14:32:45 -0500

    fl2-gxd/GXD Secondary Triage

--- fl2-gxd/skipjournals.py
+++ fl2-gxd/skipjournals.py
@@ -63,7 +63,7 @@ for r in results:
 	db.commit()
 
 results = db.sql('select count(*) as count from voc_term where _vocab_key = 184', 'auto')
-print(str(results['']))
+print(results)
 
 results = db.sql('''
 select r.*

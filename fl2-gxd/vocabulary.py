eb84902e3364078d10c95d2884bb65ee8079d488
Lori Corbani <lori.corbani@jax.org>
2022-11-18 12:44:52 -0500

    fl2-gxd/GXD Secondary Triage

--- fl2-gxd/vocabulary.py
+++ fl2-gxd/vocabulary.py
@@ -18,6 +18,7 @@ db.useOneConnection(1)
 
 inFile = open('cat1Exclude.txt', 'r')
 seqNum = 1
+db.sql('delete from VOC_Term where _vocab_key = 135)',None)
 for line in inFile.readlines():
 	term = line[:-1].replace("'","''")
 	addSQL = '''
@@ -30,7 +31,50 @@ for line in inFile.readlines():
 	db.commit()
 inFile.close()
 
-#(select _Vocab_key from VOC_Vocab where name = 'Workflow Tag'),'%s',null,null,%d,0,1001,1001,now(),now());
+inFile = open('ageExclude.txt', 'r')
+seqNum = 1
+db.sql('delete from VOC_Term where _vocab_key = 181)',None)
+for line in inFile.readlines():
+	term = line[:-1].replace("'","''")
+	addSQL = '''
+	insert into VOC_Term values(
+	(select max(_Term_key) + 1 from VOC_Term),181,'%s',null,null,%d,0,1001,1001,now(),now());
+	''' % (term, seqNum)
+	seqNum += 1
+	print(addSQL)
+	db.sql(addSQL, None)
+	db.commit()
+inFile.close()
+
+inFile = open('cat2Exclude.txt', 'r')
+seqNum = 1
+db.sql('delete from VOC_Term where _vocab_key = 182)',None)
+for line in inFile.readlines():
+	term = line[:-1].replace("'","''")
+	addSQL = '''
+	insert into VOC_Term values(
+	(select max(_Term_key) + 1 from VOC_Term),182,'%s',null,null,%d,0,1001,1001,now(),now());
+	''' % (term, seqNum)
+	seqNum += 1
+	print(addSQL)
+	db.sql(addSQL, None)
+	db.commit()
+inFile.close()
+
+inFile = open('cat2Terms.txt', 'r')
+seqNum = 1
+db.sql('delete from VOC_Term where _vocab_key = 183)',None)
+for line in inFile.readlines():
+	term = line[:-1].replace("'","''")
+	addSQL = '''
+	insert into VOC_Term values(
+	(select max(_Term_key) + 1 from VOC_Term),183,'%s',null,null,%d,0,1001,1001,now(),now());
+	''' % (term, seqNum)
+	seqNum += 1
+	print(addSQL)
+	db.sql(addSQL, None)
+	db.commit()
+inFile.close()
 
 db.useOneConnection(0)
 

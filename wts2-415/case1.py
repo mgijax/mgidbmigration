719dbbc0b4818692d9beb09c972ff4aea144027a
Lori Corbani <lori.corbani@jax.org>
2023-08-30 10:19:09 -0400

    wts2-415/fl2-495/No Phenoypic Analysis migration (TR12775)

--- wts2-415/case1.py
+++ wts2-415/case1.py
@@ -41,10 +41,37 @@ order by c.jnumid, aa.symbol
 updateSQL = ''
 for r in results:
         print(r)
-        updateSQL += "update mgi_reference_assoc set _refassoctype_key = 1014 where _assoc_key = " + str(r['_assoc_key']) + ";\n"
+        updateSQL += "update mgi_reference_assoc set _refassoctype_key = 1014, _modifiedby_key = 1454, modification_date = now() where _assoc_key = " + str(r['_assoc_key']) + ";\n"
 
 print(updateSQL)
-#db.sql(updateSQL, None)
-#db.commit()
+db.sql(updateSQL, None)
+db.commit()
+print(len(results))
+
+print('\nafter the Not Used update\n')
+results = db.sql('''
+select distinct c.jnumid, aa.symbol, mrt.assoctype, s.strain, mr._assoc_key
+from voc_annot v, voc_evidence e,
+bib_citation_cache c, gxd_genotype g, prb_strain s, gxd_allelegenotype ga, all_allele aa,
+mgi_reference_assoc mr, mgi_refassoctype mrt
+where v._annottype_key = 1002
+and v._term_key = 293594
+and v._annot_key = e._annot_key
+and e._refs_key = c._refs_key
+and v._object_key = g._genotype_key
+and g._strain_key = s._strain_key
+and g._genotype_key = ga._genotype_key
+and ga._allele_key = aa._allele_key
+and aa._allele_key = mr._object_key
+and mr._mgitype_key = 11
+and e._refs_key = mr._refs_key
+and mr._refassoctype_key = mrt._refassoctype_key
+and mrt._refassoctype_key = 1014
+order by c.jnumid, aa.symbol
+''', 'auto')
+for r in results:
+        print(r)
+print(len(results))
+
 print('\n\ncase 1 end: Change the reference type from USED-FC to Not Used\n\n')
 

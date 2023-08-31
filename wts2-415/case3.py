dbe02766b653e6fabc81fa98d6d3be49afc53da7
Lori Corbani <lori.corbani@jax.org>
2023-08-31 08:32:55 -0400

    wts2-415/fl2-495/No Phenoypic Analysis migration (TR12775)

--- wts2-415/case3.py
+++ wts2-415/case3.py
@@ -46,7 +46,34 @@ for r in results:
 
 print(deleteSQL)
 print(len(results))
-#db.sql(deleteSQL, None)
-#db.commit()
+db.sql(deleteSQL, None)
+db.commit()
+
+results = db.sql('''
+select distinct a.accid, substring(n.note,1,100) as note, s.strain, c.jnumid, v._annot_key
+from voc_annot v, voc_evidence e, 
+bib_citation_cache c, gxd_genotype g, prb_strain s, mgi_note n,
+acc_accession a
+where v._annottype_key = 1002
+and v._term_key = 293594
+and v._annot_key = e._annot_key
+and e._refs_key = c._refs_key
+and v._object_key = g._genotype_key
+and g._strain_key = s._strain_key
+and g._genotype_key = n._object_key
+and n._notetype_key = 1016
+and g._genotype_key = a._object_key
+and a._mgitype_key = 12
+and a.prefixpart = 'MGI:'
+and a._logicaldb_key = 1
+and not exists (select 1 from voc_annot vv 
+        where v._object_key = vv._object_key
+        and vv._annottype_key = 1002
+        and v._term_key = vv._term_key
+        and vv._term_key != 293594
+        )
+order by note
+''', 'auto')
+print(len(results))
 
 print('\n\ncase 3 end: delete all "no phenotypic analysis (MP:0003012)" annotations\n\n')

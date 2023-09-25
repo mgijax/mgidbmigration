2eb08f90be00e395941178076b2506d17b3e9e30
Lori Corbani <lori.corbani@jax.org>
2023-09-25 13:14:25 -0400

    wts2-393/Disease to reference annotation

--- wts2-393/wts393.py
+++ wts2-393/wts393.py
@@ -9,13 +9,15 @@ lineNum = 0
 for line in inFile.readlines():
     lineNum = lineNum + 1
     tokens = line[:-1].split('\t')
-    jnumid = 'J:' + tokens[0]
+    jnumid = tokens[0]
     note = tokens[1]
 
     results = db.sql(''' select _refs_key from bib_citation_cache where jnumid = '%s' ''' % (jnumid), 'auto')
 
     for r in results:
-        cmd = ''' insert into BIB_Notes value(%s, '%s', now(), now()); ''' % (r['_refs_key'], note)
+        db.sql('delete from BIB_Notes where _refs_key = %s' % (r['_refs_key']), None)
+        db.commit()
+        cmd = ''' insert into BIB_Notes values(%s, '%s', now(), now()); ''' % (r['_refs_key'], note)
         db.sql(cmd, None)
         db.commit()
 

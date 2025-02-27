508e273e9fd028931f8a23f026e5a859ed2723cc
Lori Corbani <lori.corbani@jax.org>
2025-02-27 11:09:56 -0500

    wts2-1392/e4g-155/ENSEMBL Regulatory 113

---
+++ ensembl113/marker.py
@@ -0,0 +1,49 @@
+#
+# make the Ensembl Reg 108 ID an Exact Synonym of the Marker
+#
+
+import sys
+import os
+import db
+import mgi_utils
+
+synFileName = 'MRK_Synonym.bcp'
+synFile = open(synFileName, 'w')
+
+results = db.sql(''' select nextval('mgi_synonym_seq') as maxKey ''', 'auto')
+synKey = results[0]['maxKey']
+
+results = db.sql('''
+select m.symbol, s.*
+from seq_marker_cache s, mrk_marker m
+where s._logicaldb_key = 222
+and s._marker_key = m._marker_key
+and exists (select 1 from all_allele a where a._marker_key = s._marker_key )
+''', 'auto')
+
+mgiTypeKey = 2
+synTypeKey = 1004
+referenceKey = 551295       # J:321631
+createdByKey = 1093
+cdate = mgi_utils.date('%m/%d/%Y')  # current date
+
+for r in results:
+    print(r)
+    markerKey = r['_marker_key']
+    esId = r['accid']
+    
+    synFile.write('%d|%d|%d|%d|%s|%s|%s|%s|%s|%s\n' \
+        % (synKey, markerKey, mgiTypeKey, synTypeKey, referenceKey, esId, createdByKey, createdByKey, cdate, cdate))
+    synKey = synKey + 1
+
+synFile.close()
+bcpCommand = os.environ['PG_DBUTILS'] + '/bin/bcpin.csh'
+currentDir = os.getcwd()
+bcp = '%s %s %s %s %s %s "|" "\\n" mgd' % \
+    (bcpCommand, db.get_sqlServer(), db.get_sqlDatabase(), 'MGI_Synonym', currentDir, synFileName)
+
+os.system(bcp)
+
+db.sql(''' select setval('mgi_synonym_seq', (select max(_Synonym_key) from MGI_Synonym)) ''', None)
+db.commit()
+
---
+++ ensembl113/wts1-1392.csh
@@ -0,0 +1,34 @@
+#!/bin/csh -f
+
+#
+# Template
+#
+
+
+if ( ${?MGICONFIG} == 0 ) then
+        setenv MGICONFIG /usr/local/mgi/live/mgiconfig
+endif
+
+source ${MGICONFIG}/master.config.csh
+
+cd `dirname $0`
+
+setenv LOG $0.log
+rm -rf $LOG
+touch $LOG
+ 
+date | tee -a $LOG
+ 
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+
+EOSQL
+
+# make the Ensembl Reg 108 ID an Exact Synonym of the Marker
+$PYTHON marker.py | tee -a $LOG
+
+# delete Ensembl Reg 108
+# delete Ensembl Reg 108 Markers that do not have Alleles, or any other object
+#./delete108.csh | tee -a $LOG
+
+date |tee -a $LOG
+

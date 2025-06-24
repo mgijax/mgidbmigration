32797fd2412d000a4ded2e8ca631983f988f414c
Lori Corbani <lori.corbani@jax.org>
2025-06-24 13:28:54 -0400

    wts2-1689/e4g-298/have GXDHT litriage status reflect HT Index curation status = done

--- wts2-1689/cleanup.py
+++ wts2-1689/cleanup.py
@@ -26,7 +26,7 @@ updateSQL1 = ""
 updateSQL3 = ""
 
 # If a PMID has an associated done HT Index Experiment, its GXDHT LitTriage status should = Indexed.
-results = db.sql('''
+query1 = '''
 select c._refs_key, c.pubmedid, c.jnumid, a.accid, t1.term as curationStatus, t2.term as workflowStatus
 from GXD_HTExperiment e, MGI_Property p, BIB_Citation_Cache c, BIB_Workflow_Status b, ACC_Accession a, VOC_Term t1, VOC_Term t2
 where e._CurationState_key = 20475421
@@ -41,18 +41,18 @@ and b._Status_key != 31576673
 and e._Experiment_key = a._object_key and a._mgitype_key = 42
 and e._CurationState_key = t1._term_key
 and b._Status_key = t2._term_key
-''', 'auto')
+'''
+results = db.sql(query1, 'auto')
 
 print('\n\n1: If a PMID has an associated done HT Index Experiment, its GXDHT LitTriage status should = Indexed')
 print('rows:', str(len(results)))
-
 for r in results:
     print(r)
     key = r['_refs_key']
     updateSQL1 += '''update BIB_Workflow_Status w set isCurrent = 0 where w._Group_key = 114000000 and w._refs_key = %s;\n''' % (key)
     updateSQL1 += '''insert into BIB_Workflow_Status values(nextval('bib_workflow_status_seq'),%s,114000000,31576673,1,1001,1001,now(),now());\n''' % (key)
 
-results = db.sql('''
+query2 = '''
 select c._refs_key, c.pubmedid, c.jnumid, a.accid, t1.term as curationStatus, t2.term as workflowStatus
 from GXD_HTExperiment e, MGI_Property p, BIB_Citation_Cache c, BIB_Workflow_Status b, ACC_Accession a, VOC_Term t1, VOC_Term t2
 where e._CurationState_key != 20475421
@@ -67,7 +67,8 @@ and b._Status_key = 31576673
 and e._Experiment_key = a._object_key and a._mgitype_key = 42
 and e._CurationState_key = t1._term_key
 and b._Status_key = t2._term_key
-''', 'auto')
+'''
+results = db.sql(query2, 'auto')
 
 print('\n\n2: If a PMID has been set to GXDHT=Indexed and it is not done, then its status should be changed to Chosen.')
 print('rows:', str(len(results)))
@@ -78,7 +79,7 @@ for r in results:
 # PMIDs whose GXDHT status=Not Routed should be set to New. 
 # (This is because this data type is not included in the LitTriage Classifier;
 # I picked an incorrect status when we set the system up originally.)
-results = db.sql('''
+query3 = '''
 select c._refs_key, c.pubmedid, c.jnumid, a.accid, t1.term as curationStatus, t2.term as workflowStatus
 from GXD_HTExperiment e, MGI_Property p, BIB_Citation_Cache c, BIB_Workflow_Status b, ACC_Accession a, VOC_Term t1, VOC_Term t2
 where e._Experiment_key = p._Object_key
@@ -92,7 +93,8 @@ and b._Status_key = 31576669
 and e._Experiment_key = a._object_key and a._mgitype_key = 42
 and e._CurationState_key = t1._term_key
 and b._Status_key = t2._term_key
-''', 'auto')
+'''
+results = db.sql(query3, 'auto')
 
 print('\n\n3: PMIDs whose GXDHT status=Not Routed should be set to New.')
 print('rows:', str(len(results)))
@@ -112,3 +114,12 @@ if len(updateSQL3) > 0:
     db.sql(updateSQL3, None)
     db.commit()
 
+results = db.sql(query1, 'auto')
+print('\n\n1: If a PMID has an associated done HT Index Experiment, its GXDHT LitTriage status should = Indexed')
+print('rows:', str(len(results)))
+results = db.sql(query2, 'auto')
+print('\n\n2: If a PMID has been set to GXDHT=Indexed and it is not done, then its status should be changed to Chosen.')
+print('rows:', str(len(results)))
+results = db.sql(query3, 'auto')
+print('\n\n3: PMIDs whose GXDHT status=Not Routed should be set to New.')
+print('rows:', str(len(results)))
